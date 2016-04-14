55861a8c7bef8e6dad39619f1d2fa54cdc1b0ce6
Matt Pindell <pindell.m@gmail.com>
2016-04-13 21:00:07 -0600

    rudimentary file writing funcionality implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
-#[cfg(test)]
-mod test {
-    #[test]
-    fn it_works() {
-    }
+extern crate rustc_serialize;
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::io;
+use rustc_serialize::json;
+
+pub fn write_file(data: String) -> io::Result<()> {
+    let mut buffer = try!(File::create("./foo"));
+    try!(buffer.write_fmt(format_args!("{}", data)));
+    Ok(())
+}
+
+#[derive(RustcEncodable, Debug)]
+struct TableData {
+    test_string: String,
+}
+
+#[test]
+fn it_works() {
+    let mut x = TableData { test_string: String::new() };
+    x.test_string = "test me".to_string();
+    let encoded = json::encode(&x).unwrap();
+    write_file(encoded);
 }
