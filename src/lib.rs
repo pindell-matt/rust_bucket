c99a964c38aa0e039ac828d49740a23b6ed4fa89
Regis <boudinot.regis@yahoo.com>
2016-06-11 20:04:16 -0600

    works - now we need to be able to change specific data or figure out if we want to append data

--- src/lib.rs
+++ src/lib.rs
@@ -14,33 +14,21 @@ use std::path::Path;
 
 mod sc; // sc is the user defined schema
 
-pub fn add_data_to_table<T: Serialize>(table: String, t: &T) -> io::Result<()> {
+pub fn update_table<T: Serialize>(table: String, t: &T) -> io::Result<()> {
     let serialized = serde_json::to_string(t).unwrap();
-    let pre_data   = read_table(table.clone());
-    // let new_data   =
-    // let mut buffer = try!(File::create(db_table));
-    // try!(buffer.write_all(serialized.as_bytes()));
-
-    Ok(())
-}
-
-fn new_table_data<P: AsRef<Path>, T: Serialize>(table: P, t: &T) -> io::Result<()> {
-    let serialized = serde_json::to_string(t).unwrap();
-
     let db_table = Path::new("./db").join(table);
     let mut buffer = try!(File::create(db_table));
-
     try!(buffer.write_all(serialized.as_bytes()));
 
     Ok(())
 }
 
 pub fn create_table<P: AsRef<Path>, T: Serialize>(table: P, t: &T) -> io::Result<()> {
-    let serialized = serde_json::to_string(t).unwrap();
-
     create_db_dir();
 
+    let serialized = serde_json::to_string(t).unwrap();
     let db_table = Path::new("./db").join(table);
+
     if db_table.exists() { return Ok(()) };
 
     let mut buffer = try!(File::create(db_table));
@@ -51,12 +39,10 @@ pub fn create_table<P: AsRef<Path>, T: Serialize>(table: P, t: &T) -> io::Result
 
 fn create_db_dir() -> io::Result<()>{
     if Path::new("./db").exists() { return Ok(()) }
-
     match fs::create_dir("db") {
         Err(why) => println!("! {:?}", why.kind()),
         Ok(_) => {},
     };
-
     Ok(())
 }
 
@@ -64,18 +50,20 @@ pub fn read_table<P: AsRef<Path>>(table: P) -> String {
     let db_table = Path::new("./db").join(table);
     let file     = File::open(db_table).expect("Table does not exist!");
     let buf      = BufReader::new(file);
-
     buf.lines().map(|l| l.expect("Table read failure!")).collect()
 }
 
 #[test]
 fn it_can_create_a_table_and_take_any_struct_to_add_data() {
-    // Do not change this test :) ///////////////
-    let c     = sc::Coordinates {x: 42, y: 9000};
-    let t_n   = "test".to_string();
-    let t_n_t = t_n.clone();
+    let c  = sc::Coordinates {x: 42, y: 9000};
+    let c2 = sc::Coordinates {x: 32, y: 9000};
+    let c3 = sc::Coordinates {x: 42, y: 9000};
 
-    create_table(t_n, &c);
+    create_table("test".to_string(), &c);
+    assert_eq!(serde_json::to_string(&c).unwrap(), read_table("test".to_string()));
 
-    assert_eq!(serde_json::to_string(&c).unwrap(), read_table(t_n_t))
+    update_table("test".to_string(), &c2);
+    assert_eq!(serde_json::to_string(&c2).unwrap(), read_table("test".to_string()));
+
+    update_table("test".to_string(), &c3);
 }
